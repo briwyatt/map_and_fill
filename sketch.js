c5634afe939bb3ee4bc7da2f96248ea053c18d6e
Bri Wyatt <mbriwyatt@gmail.com>
2018-02-24 13:35:24 -0500

    introduce fill chain it onto map

--- sketch.js
+++ sketch.js
@@ -1,6 +1,11 @@
 // console.log("hello");
-let vals = [4, 8, 1, 2, 9];
+// let vals = [4, 8, 1, 2, 9];
+// console.log("vals: ", vals);
+
+let vals = new Array(100);
+vals = vals.fill(0).map(Math.random);
 console.log("vals: ", vals);
 
-vals = vals.map(x => x * 2);
-console.log("vals doubled: ", vals);
+
+// vals = vals.map(x => x * 2);
+// console.log("vals doubled: ", vals);
